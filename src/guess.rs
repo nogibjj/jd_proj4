0fe237d64e698519113ae877985ceba4442c676a
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-03-16 02:39:40 +0000

    changes to guess

---
+++ src/guess.rs
@@ -0,0 +1,9 @@
+// model to make guess 
+
+// make first guess 
+// always the same guess 
+
+
+// make best subsequent guess 
+// takes in letters and calcs max expectation of word or letters? 
+// finds word and returns guess 

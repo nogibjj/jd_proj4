4de6f6a17c07e8861a245e354be2c248e3a76078
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-04-05 02:58:33 +0000

    add python version

--- test/main.py
+++ test/main.py
@@ -1,18 +1,19 @@
 from wordle import play_wordle
+import pandas as pd 
 
 
 def check_allowed_words(word, word_list):
-    if word not in word_list:
-        print("Word {} not in allowed list of words", word);
+    if ~word_list["words"].str.contains(word).any():
+        print("Word " + word +  " not in allowed list of words");
         exit(1);
 
 
 def main():
-    input = ["ghost", "there", "wrote"]
-
+    input = ["ghoul", "ghost", "later"]
 
     # get list of potential words
-    word_list = ["ghost", "there", "wrote"]
+    # read in txt file to pandas dataframe
+    word_list = pd.read_csv("test/words.txt", header=None, names=["words"])
     # CHANGE TO BIG DATA SOURCE
 
     total_tries = 0;
@@ -22,11 +23,11 @@ def main():
         # check if word is in allowed list of words
         check_allowed_words(word, word_list);
 
-        guesses = play_wordle(word.upper(), word_list);
+        guesses = play_wordle(word, word_list);
         total_tries += guesses;
 
     length = len(input)
-    print("Average tries: " + str(total_tries / length));
+    print("Average tries: " + str(round(total_tries / length, 2)));
 
     return 
 
--- test/wordle.py
+++ test/wordle.py
@@ -5,17 +5,25 @@ def process_guess(true_word, guess, word_list):
 
     for i in range(len(true_word)):
         if guess[i] == true_word[i]:
-            styled = "\033[0;30;42m" + guess[i] 
-        elif guess[i] in true_word:
-            styled = "\033[0;30;43m" + guess[i] 
-        else:
-            styled = "\033[0;30;47m" + guess[i] 
+            styled = "\033[0;30;42m" + guess[i].upper() + "\033[m"
 
+            # filter word list only if the first letter is the same as guess[i]
+            word_list = word_list[word_list["words"].str[i] == guess[i]]
+        elif guess[i] in true_word:
+            styled = "\033[0;30;43m" + guess[i].upper() + "\033[m"
+
+            # filter word list for words that do not have guess[i] in the same position
+            word_list = word_list[word_list["words"].str[i] != guess[i]]
+            # filter word list for only words that contain guess[i]
+            word_list = word_list[word_list["words"].str.contains(guess[i])]
+        else:
+            styled = "\033[0;30;47m" + guess[i].upper() + "\033[m"
+
+            # filter word list for words that do not contain guess[i]
+            word_list = word_list[~word_list["words"].str.contains(guess[i])]
         styled_string += styled
     
     print(styled_string)
-            
-    # filter word list HERE 
 
     return word_list 
 
@@ -27,7 +35,7 @@ def play_wordle(true_word, word_list):
 
     while not solved:
         if guess_count == 0:
-            guess = "salet".upper()
+            guess = "salet"
         else:
             guess = get_guess(filtered_word_list)
         
